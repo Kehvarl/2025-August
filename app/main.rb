cf4d9eb925aed914ae1bd238accffe40b453c1e4
Kehvarl <Kehvarl@Kehvarl.com>
2025-08-15 20:07:13 -0700

    lots of sprites piling up

--- app/main.rb
+++ app/main.rb
@@ -1,19 +1,22 @@
 def init args
-  args.state.blocks = []
+  args.state.falling = []
+  args.state.landed = []
 end
 
 def calc_physics args
-  args.state.blocks.each do |b|
+  args.state.falling.each do |b|
     b.y -= b.vy
-    if b.y <= 0
+    if b.y <= 0 or args.geometry.find_all_intersect_rect(b, args.state.landed).any?
       b.vy = 0
+      args.state.landed << b
     end
   end
+  args.state.falling.reject!{|f| f.vy <= 0}
 end
 
 def add_block args
-  args.state.blocks << {
-    x: rand(1280), y: rand(100) + 620, w: 8, h: 8,
+  args.state.falling << {
+    x: rand(1280), y: rand(50) + 670, w: 8, h: 8,
     path: "sprites/square/blue.png",
     vy: 1
     }
@@ -29,8 +32,14 @@ def tick args
     add_block(args)
   end
 
+  if args.inputs.keyboard.c
+    puts args.state.landed.size
+  end
+
   calc_physics(args)
 
-  args.outputs.primitives << args.state.blocks
+  args.outputs.primitives << args.state.landed
+
+  args.outputs.primitives << args.state.falling
 
 end
